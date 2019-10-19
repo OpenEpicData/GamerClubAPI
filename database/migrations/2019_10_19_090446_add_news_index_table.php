b59bc5ce933f35b3d473192eb6f992b2151773d7
AGD <agdholo@gmail.com>
2019-10-19 17:17:30 +0800

    chore: add news index

---
+++ database/migrations/2019_10_19_090446_add_news_index_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewsIndexTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function(Blueprint $table){
+            $sql = <<<SQL
+                CREATE INDEX pgroonga_title_index ON news USING pgroonga (title);
+                CREATE INDEX pgroonga_description_index ON news USING pgroonga (description);
+                CREATE INDEX pgroonga_author_index ON news USING pgroonga (author);
+                CREATE INDEX pgroonga_game_name_index ON news USING pgroonga (game_name);
+            SQL;
+            DB::connection()->getPdo()->exec($sql);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function(Blueprint $table){
+            $sql = <<<SQL
+                ALTER TABLE news DROP INDEX pgroonga_title_index;
+                ALTER TABLE news DROP INDEX pgroonga_description_index;
+                ALTER TABLE news DROP INDEX pgroonga_author_index;
+                ALTER TABLE news DROP INDEX pgroonga_game_name_index;
+            SQL;
+            DB::connection()->getPdo()->exec($sql);
+        });
+    }
+}
