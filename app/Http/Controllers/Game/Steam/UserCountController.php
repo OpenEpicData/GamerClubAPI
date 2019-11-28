9cd9ac2fa7aff4d94c4f428148f08347f1d12e8c
AGD <agdholo@gmail.com>
2019-11-28 14:26:46 +0800

    feat: add user count status query

--- app/Http/Controllers/Game/Steam/UserCountController.php
+++ app/Http/Controllers/Game/Steam/UserCountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Game\Steam;
 use App\Http\Controllers\Controller;
 use App\Http\Model\Game\Steam\UserCount;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Arr;
 
 class UserCountController extends Controller
 {
@@ -17,9 +18,18 @@ class UserCountController extends Controller
         $user = $data->pluck('user');
         $created_at = $data->pluck('created_at');
 
+        $avg = $data->avg('user');
+        $max = $data->max('user');
+        $min = $data->min('user');
+        $now = $user->last();
+
         return [
             'user' => $user,
-            'created_at' => $created_at
+            'created_at' => $created_at,
+            'avg' => $avg,
+            'max' => $max,
+            'min' => $min,
+            'now' => $now
         ];
     }
 }
