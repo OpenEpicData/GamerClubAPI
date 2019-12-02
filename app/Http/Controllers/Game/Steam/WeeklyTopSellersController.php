c7710c3479b1902ce8036cf8a23604adc2b087d9
AGD <agdholo@gmail.com>
2019-12-02 14:29:08 +0800

    fix: correct last sunday query

--- app/Http/Controllers/Game/Steam/WeeklyTopSellersController.php
+++ app/Http/Controllers/Game/Steam/WeeklyTopSellersController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Game\Steam;
 
 use App\Http\Controllers\Controller;
 use App\Http\Model\Game\Steam\WeeklyTopSellers;
+use Carbon\CarbonInterval;
 use Illuminate\Support\Carbon;
 
 class WeeklyTopSellersController extends Controller
 {
     public function index() {
-        return WeeklyTopSellers::whereDate('created_at', Carbon::today())
+        $start = Carbon::now()->startOfWeek()->sub(CarbonInterval::days(1));
+        return WeeklyTopSellers::whereDate('created_at', $start)
             ->oldest('rank')
             ->get();
     }
