53596bc6dadec0358f3fde2b8ed2a7c4e27530e7
AGD <agdholo@gmail.com>
2020-03-12 00:43:47 +0800

    chore: add subweek functions in weekly top seller

--- app/Http/Controllers/Game/Steam/WeeklyTopSellersController.php
+++ app/Http/Controllers/Game/Steam/WeeklyTopSellersController.php
@@ -6,11 +6,13 @@ use Carbon\CarbonInterval;
 use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
 use App\Http\Model\Game\Steam\WeeklyTopSellers;
+use Illuminate\Http\Request;
 
 class WeeklyTopSellersController extends Controller
 {
-    public function index() {
-        $start = Carbon::now()->startOfWeek()->sub(CarbonInterval::days(1));
+    public function index(Request $request) {
+        $subWeek = $request->subWeek ?? 0;
+        $start = Carbon::now()->startOfWeek()->subWeek($subWeek)->sub(CarbonInterval::days(1));
         return WeeklyTopSellers::whereDate('created_at', $start)
             ->oldest('rank')
             ->get();
